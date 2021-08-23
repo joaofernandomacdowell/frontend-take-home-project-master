a411935e46e19b656e2844d63fefb262739523ce
João Fernando <jfmcdowel2@gmail.com>
2021-08-23 16:23:39 -0300

    Improve pagination layout

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,6 +1,7 @@
 // eslint-disable @typescript-eslint/explicit-module-boundary-types
 import { useState, useCallback } from 'react';
 
+// TODO: Uses in Pagination to set page
 export default function useToggle(
   initialValue = false
 ): (boolean | (() => void))[] {
