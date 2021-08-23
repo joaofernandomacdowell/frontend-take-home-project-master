1089f1f554978e849f358f106b9607336f23c6d0
João Fernando <jfmcdowel2@gmail.com>
2021-08-23 11:17:39 -0300

    Fix search & pagination

--- src/pages/Countries/Countries.tsx
+++ src/pages/Countries/Countries.tsx
@@ -12,48 +12,8 @@ import { Country } from '../../api/types';
 const ITEMS_PER_PAGE = 20;
 
 interface CountriesProps {
-  filteredCountries: Country[];
+  countries: Country[];
 }
-
-const Countries = ({
-  filteredCountries,
-}: CountriesProps): JSX.Element => {
-  const dispatch = useDispatch();
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [text, setText] = useState('');
-
-  // Execute when input searchText has changed
-  useEffect(() => {
-    dispatch(setSearchCountry(text));
-  }, [dispatch, text]);
-
-  const indexOfLastCountry = currentPage * ITEMS_PER_PAGE;
-  const indexOfFirstCountry = indexOfLastCountry - ITEMS_PER_PAGE;
-  const currentCountries = filteredCountries.slice(
-    indexOfFirstCountry,
-    indexOfLastCountry
-  );
-
-  console.log('filteredCountries: ', filteredCountries);
-  console.log('currentCountries: ', currentCountries);
-  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
-
-  return (
-    <>
-      <Search
-        text={text}
-        onChange={(searchTerm: string) => setText(searchTerm)}
-      />
-      <CountriesList countries={currentCountries} />
-      <Pagination
-        itemsPerPage={ITEMS_PER_PAGE}
-        totalItems={filteredCountries.length}
-        paginate={paginate}
-      />
-    </>
-  );
-};
 
 const filterFn = (countries: Country[], text: string) =>
   countries.filter((country) => {
@@ -66,15 +26,58 @@ const filterFn = (countries: Country[], text: string) =>
     return false;
   });
 
-const mapStateToProps = ({
-  countriesState,
-  searchState,
-}: AppState) => {
+const Countries = ({ countries }: CountriesProps): JSX.Element => {
+  const dispatch = useDispatch();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [text, setText] = useState('');
+
+  const indexOfLastCountry = currentPage * ITEMS_PER_PAGE;
+  const indexOfFirstCountry = indexOfLastCountry - ITEMS_PER_PAGE;
+  const currentCountries = countries.slice(
+    indexOfFirstCountry,
+    indexOfLastCountry
+  );
+
+  const [filtered, setFiltered] = useState(currentCountries);
+
+  // Execute when input searchText has changed
+  useEffect(() => {
+    console.log('useEffect');
+    dispatch(setSearchCountry(text.trim()));
+    setFiltered(filterFn(countries, text));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [text]);
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  return (
+    <>
+      <Search
+        text={text}
+        onChange={(searchTerm: string) => setText(searchTerm)}
+      />
+      <CountriesList
+        countries={
+          filtered.length === countries.length
+            ? currentCountries
+            : filtered
+        }
+      />
+      <Pagination
+        itemsPerPage={ITEMS_PER_PAGE}
+        totalItems={filtered.length}
+        paginate={paginate}
+      />
+    </>
+  );
+};
+
+const mapStateToProps = ({ countriesState }: AppState) => {
   const { countries } = countriesState;
-  const { text } = searchState;
 
   return {
-    filteredCountries: filterFn(countries, text),
+    countries,
   };
 };
 
